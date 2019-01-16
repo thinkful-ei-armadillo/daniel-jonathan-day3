5dc563db12cabbd4950379f166af7e700d3d32f3
Jonathan Kleinpeter <joklein341@gmail.com>
2019-01-16 11:04:33 -0600

    add Entroll in summer school

--- object-drills/object-drills-redo.js
+++ object-drills/object-drills-redo.js
@@ -62,3 +62,29 @@ function makeStudentsReport(data) {
 
 // enroll in summer school
 
+const studentData = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
+];
+
+function enrollInSummerSchool(students) {
+  for (let i = 0; i < students.length; i++) {
+    students[i].status = 'In Summer school';
+  }
+  return students;
+}
+
+// Find by id
