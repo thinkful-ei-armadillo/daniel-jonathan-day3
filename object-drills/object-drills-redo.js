f986894d8023cf03119de882066fc97b6061e2a4
Jonathan Kleinpeter <joklein341@gmail.com>
2019-01-16 11:22:42 -0600

    final drills 2 commit

--- object-drills/object-drills-redo.js
+++ object-drills/object-drills-redo.js
@@ -107,3 +107,33 @@ function findById(items, idNum) {
 
 // validate obj keys
 
+// running the function with `objectA` and `expectedKeys`
+// should return `true`
+const objectA = {
+  id: 2,
+  name: 'Jane Doe',
+  age: 34,
+  city: 'Chicago',
+};
+
+// running the function with `objectB` and `expectedKeys`
+// should return `false`
+const objectB = {
+  id: 3,
+  age: 33,
+  city: 'Peoria',
+};
+
+const expectedKeys = ['id', 'name', 'age', 'city'];
+
+function validateKeys(object, expectedKeys) {
+  let length = Object.keys(object).length;
+  let keys = Object.keys(object);
+  for (let i = 0; i < expectedKeys.length; i++) {
+    if (length === expectedKeys.length && keys[i] === expectedKeys[i]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
