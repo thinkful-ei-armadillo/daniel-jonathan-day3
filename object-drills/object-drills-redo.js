9e87d6fc0aad9b242a5b6b2fd933bc3dc44a7597
Daniel Ors <do7811@gmail.com>
2019-01-16 11:43:15 -0500

    added and solved personMaker drill

--- object-drills/object-drills-redo.js
+++ object-drills/object-drills-redo.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// obj creator
 function createMyObject() {
   const objectResult = {
     foo: 'bar',
@@ -12,7 +12,7 @@ function createMyObject() {
   return objectResult;
 }
 
-// 
+// obj updater
 function updateObject(obj) {
   obj.foo = 'foo',
   obj.bar = 'bar',
@@ -20,33 +20,19 @@ function updateObject(obj) {
   obj.bang = 'bang'
   return obj;
 }
+// self reference drill
 
-(function testUpdateObject() {
-  var oldObj = {
-    cats: 'cats',
-    dogs: 'dogs',
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function () {
+      return `${this.firstName} ${this.lastName}`;
+    },
   };
-  var newObj = updateObject(oldObj);
-  if (typeof newObj !== 'object') {
-    console.error('ERROR: `createMyObject` must return an object');
-    return false
-  }
-  ['foo', 'bar', 'bizz', 'bang'].forEach(function (key) {
-    if (!(key in newObj)) {
-      console.error('ERROR: `' + key + '` not in object returned by `updateObject`');
-      return false;
-    }
-  });
-  ['foo', 'bar', 'bizz', 'bang'].forEach(function (key) {
-    if (newObj[key] !== key) {
-      console.error('ERROR: `' + key + '` should be "' + key + '" but was ' + newObj[key]);
-      return false;
-    }
-  });
-  if (!(newObj.cats === 'cats' && newObj.dogs === 'dogs')) {
-    console.error('ERROR: your function doesn\'t preserve existing key/value pairs');
-    return false;
-  }
-  console.log('SUCCESS: `updateObject` works correctly!');
+  return person;
+}
 
-})();
+// 
